9b0885e1a06d35d8f2850a7ec79808ce58b9c287
mdiph <dhivapradigta96@gmail.com>
2022-11-23 11:00:51 +0700

    Adding About Component

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -1,16 +1,27 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 
-const image = 'https://awsimages.detik.net.id/community/media/visual/2021/04/22/5-makanan-enak-dari-indonesia-dan-malaysia-yang-terkenal-enak-5.jpeg?w=700&q=90'
 
-const title = 'Warteg Gunawan 27'
-const description = 'Indonesian '
+const yelpRestaurantInfo = {
+    name : "Ayam Goreng Spesial Gunawan",
+    image: "https://awsimages.detik.net.id/community/media/visual/2021/04/22/5-makanan-enak-dari-indonesia-dan-malaysia-yang-terkenal-enak-5.jpeg?w=700&q=90",
+    price: "$$",
+    reviews: "200",
+    rating: 4.5,
+    categories: [{title: 'Indonesian'}, {title: 'Murah'}],
+};
+
+const {name, image, price, reviews, rating, categories} = yelpRestaurantInfo
+
+const formattedCategories = categories.map((cat) => cat.title).join(' - ');
+
+const description = `${formattedCategories} ${price ? ' • ' + price: ''} • 🎫 • ${rating} ⭐ (${reviews}+) `
 
 export default function About() {
   return (
     <View>
       <RestaurantImage image = {image}/>
-      <RestaurantTitle title = {title}/>
+      <RestaurantName name = {name}/>
       <RestaurantDescription description = {description}/>
     </View>
   )
@@ -20,7 +31,7 @@ const RestaurantImage = (props) => (
     <Image source ={{uri: props.image}} style = {{width: "100%", height: 180,}}/>
 );
 
-const RestaurantTitle = (props) => (
+const RestaurantName = (props) => (
     <Text style = {{
         fontSize: 25,
         fontWeight: "600",
