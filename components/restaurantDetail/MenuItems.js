d155793c229e0d32c54b4e2b0874142e557c1996
mdiph <dhivapradigta96@gmail.com>
2022-11-24 14:44:08 +0700

    Adding Retaining Function

--- components/restaurantDetail/MenuItems.js
+++ components/restaurantDetail/MenuItems.js
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
 import React from 'react'
 import { Divider } from 'react-native-elements';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 const foods = [
@@ -65,6 +65,13 @@ export default function MenuItems({restaurantName}) {
     },
   })
 
+  const cartItems = useSelector(
+    (state) => state.cartReducer.selectedItems.items
+    )
+
+  const isFoodInCart = (food, cartItems) => 
+    Boolean(cartItems.find((item) => item.title === food.title))
+
   return (
     <ScrollView showsVerticalScrollIndicator ={false}>
       {foods.map((food, index) => (
@@ -74,6 +81,7 @@ export default function MenuItems({restaurantName}) {
               iconStyle= {{borderColor: 'lightgray'}}
               fillColor="green"
               onPress={(checkboxValue) => selectItem(food,checkboxValue)}
+              isChecked = {isFoodInCart(food, cartItems)}
             />
             <FoodInfo food={food}/>
             <FoodImage food={food}/>
